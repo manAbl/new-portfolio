9789ff9b70fff751e3ebc34d02d4530bc88dcbae
Manuel Blanco <manuel.22.lamas@gmail.com>
2019-08-22 16:01:22 -0500

    contact page basic layout

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,47 @@
 <template>
   <div class="container mx-auto main-wrapper grid">
-    Contact forms here
+    <!-- <svg
+      id="contact-svg"
+      xmlns:svg="http://www.w3.org/2000/svg"
+      xmlns="http://www.w3.org/2000/svg"
+      width="100mm"
+      height="100vh"
+      viewBox="0 0 297 210"
+      version="1.1"
+    >
+      <g transform="translate(0,-87)">
+        <path
+          style="fill:#333333;stroke:none;stroke-width:0.26067781px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+          d="m 154.32402,69.130951 c 0,0 -44.30787,35.109779 -20.19475,52.957249 24.11313,17.84747 35.56686,31.01363 4.2198,39.2059 -31.34707,8.19229 -18.68767,32.47655 -9.04242,43.00948 9.64525,10.53293 37.97817,30.72106 22.60605,44.47238 -15.37212,13.75133 -28.93575,19.31038 -20.19474,37.743 8.74101,18.43263 1.50707,22.2362 1.50707,22.2362 l 176.93008,-5.85164 c 0,0 1.50707,-232.309662 0.30141,-232.309662 -1.20565,0 -156.1325,-1.462907 -156.1325,-1.462907 z"
+        />
+      </g>
+    </svg> -->
+    <div class="left">
+      <h3 class="text-gray font-thin">Contact</h3>
+      <h1 class="title xl">
+        Let's get in touch<span class="text-red">:</span>
+      </h1>
+      <div class="information">
+        <h3 class="text-base text-red">
+          Contact Details:
+        </h3>
+        <a
+          href="mailto:manuel.22.lamas@gmail.com"
+          class="font-thin text-md block margin1"
+        >
+          manuel.22.lamas@gmail.com
+        </a>
+        <a
+          href="tel: +57 305 3648811"
+          class="font-thin text-md block margin-top-half-rem"
+        >
+          +57 305 3648811
+        </a>
+      </div>
+    </div>
+    <div class="right">
+      Something here
+    </div>
   </div>
 </template>
 
