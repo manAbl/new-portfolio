a4a50a4a9cdb330dd4ef4e72056dfa9c1d274e28
Manuel Blanco <manuel.22.lamas@gmail.com>
2019-09-02 08:43:39 -0500

    ignoring .nuxt folder

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['pages'] = require('..\\middleware\\pages.js');
-middleware['pages'] = middleware['pages'].default || middleware['pages']
-
-export default middleware
